364091bb871e5db6ac76e37f126ad9e274b1d34e
psyafter <psyafter@gmail.com>
2015-03-01 19:31:33 +0200

    Disable Camera replacement by kk stock camera
    Change-Id: I3da4b924d02112f89659bd4e0657dc6facb7d9cf

--- validus.mk
+++ validus.mk
@@ -36,8 +36,8 @@ PRODUCT_NAME := validus_i9100
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-I9100
 
-PRODUCT_COPY_FILES += \
-    device/samsung/i9100/prebuilt/Camera2.apk:system/addon.d/Camera2.apk
+#PRODUCT_COPY_FILES += \
+#    device/samsung/i9100/prebuilt/Camera2.apk:system/addon.d/Camera2.apk
 
 # Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9100 TARGET_DEVICE=GT-I9100 BUILD_FINGERPRINT=samsung/GT-I9100/GT-I9100:4.1.2/JZO54K/I9100XWMS2:user/release-keys PRIVATE_BUILD_DESC="GT-I9100-user 4.1.2 JZO54K I9100XWMS2 release-keys"
