9173d0bd847be1485400d91777a39d000eb0c6aa
psyafter <psyafter@gmail.com>
2015-04-02 12:31:45 +0300

    Remove unnecessary properties

--- i9100.mk
+++ i9100.mk
@@ -31,8 +31,8 @@ $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240
-    camera2.portability.force_api=1
-    net.tethering.noprovisioning=true
+#    camera2.portability.force_api=1
+#    net.tethering.noprovisioning=true
 
 # Sensors
 PRODUCT_PACKAGES += \
