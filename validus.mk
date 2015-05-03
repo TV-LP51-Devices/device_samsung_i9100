98d954dc42de588de34757a0d363e5fbd02c37fa
psyafter <psyafter@gmail.com>
2015-05-03 18:21:48 +0300

    Ringtones
    Change-Id: I65ebe9df098e0b45900d90a7044188dabf766fb3

--- validus.mk
+++ validus.mk
@@ -36,8 +36,20 @@ PRODUCT_NAME := validus_i9100
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-I9100
 
-#PRODUCT_COPY_FILES += \
-#    device/samsung/i9100/prebuilt/Camera2.apk:system/addon.d/Camera2.apk
+PRODUCT_COPY_FILES += \
+#	device/samsung/i9100/prebuilt/Camera2.apk:system/addon.d/Camera2.apk \
+	device/samsung/i9100/prebuilt/system/media/audio/notifications/!Dexter_Sms_Qsy.mp3:system/media/audio/notifications/!Dexter_Sms_Qsy.mp3 \
+	device/samsung/i9100/prebuilt/system/media/audio/notifications/!Electronic_Ring.mp3:system/media/audio/notifications/!Electronic_Ring.mp3 \
+	device/samsung/i9100/prebuilt/system/media/audio/notifications/!Exclusive_Sms.mp3:system/media/audio/notifications/!Exclusive_Sms.mp3 \
+	device/samsung/i9100/prebuilt/system/media/audio/notifications/!Excuse_Me_Boss.mp3:system/media/audio/notifications/!Excuse_Me_Boss.mp3 \
+	device/samsung/i9100/prebuilt/system/media/audio/notifications/!Facebook_Message.mp3:system/media/audio/notifications/!Facebook_Message.mp3 \
+	device/samsung/i9100/prebuilt/system/media/audio/notifications/!Galaxy_S5_Sms.mp3:system/media/audio/notifications/!Galaxy_S5_Sms.mp3 \
+	device/samsung/i9100/prebuilt/system/media/audio/notifications/!Intel.mp3:system/media/audio/notifications/!Intel.mp3 \
+	device/samsung/i9100/prebuilt/system/media/audio/notifications/!ios7_Cute_Sms.mp3:system/media/audio/notifications/!ios7_Cute_Sms.mp3 \
+	device/samsung/i9100/prebuilt/system/media/audio/notifications/!message_tone.mp3:system/media/audio/notifications/!message_tone.mp3 \
+	device/samsung/i9100/prebuilt/system/media/audio/notifications/!Nokia_Message.mp3:system/media/audio/notifications/!Nokia_Message.mp3 \
+	device/samsung/i9100/prebuilt/system/media/audio/notifications/!Spica.ogg:system/media/audio/notifications/!Spica.ogg \
+	device/samsung/i9100/prebuilt/system/media/audio/notifications/!Windows.mp3:system/media/audio/notifications/!Windows.mp3
 
 # Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9100 TARGET_DEVICE=GT-I9100 BUILD_FINGERPRINT=samsung/GT-I9100/GT-I9100:4.1.2/JZO54K/I9100XWMS2:user/release-keys PRIVATE_BUILD_DESC="GT-I9100-user 4.1.2 JZO54K I9100XWMS2 release-keys"
